0ccd12a3488555cf95f720073429cc188cce70d6
Sw4nkyp4nts69 <jeppesen.nicolai@gmail.com>
2023-02-25 13:26:03 +0100

    Check enemy in FOV range
    Fik lavet check enemy in FOV Range og prøvede at lave taskgottotarget hvilket ikke gik særligt godt, og virker ikke. Jeg har brugt alt for langt tid på at fikse fejl indtil nu så jeg aflevere.

--- Behavior tree game/Assets/Scripts/Tree sripts/GuardBT.cs
+++ Behavior tree game/Assets/Scripts/Tree sripts/GuardBT.cs
@@ -4,13 +4,27 @@ using BehaviorTree;
 
 public class GuardBT : Tree
 {
+    public static float fovRange = 6f;
+
     public UnityEngine.Transform[] waypoints;
 
     public static float speed = 0.5f;
 
     protected override Node SetupTree()
     {
-        Node root = new TaskPatrol(transform, waypoints);
+        Node root = new Selector(new List<Node>
+        {
+            new Sequence(new List<Node>
+            {
+                new CheckEnemyInFOVRange(transform),
+                new TaskGoToTarget(transform),
+            }),
+            new TaskPatrol(transform, waypoints),
+        });
+
         return root;
     }
+
+
+
 }
